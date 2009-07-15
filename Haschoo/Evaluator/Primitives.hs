48dc6684bd52814b6e10a1f06bc52004d7888425
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2009-07-15 20:34:24 +0300

    Lift all numbers to a common type for primitives.
    Ugly as hell.

--- Haschoo/Evaluator/Primitives.hs
+++ Haschoo/Evaluator/Primitives.hs
@@ -1,26 +1,52 @@
 -- File created: 2009-07-15 11:00:10
 
+{-# LANGUAGE Rank2Types #-}
+
 module Haschoo.Evaluator.Primitives (primitives) where
 
 import Data.List (foldl')
 
-import Haschoo.ScmValue (ScmValue(..))
+import Haschoo.ScmValue (ScmValue(..), isNumeric, liftScmFrac2, liftScmNum2)
 
 primitives :: [(String, ScmValue)]
-primitives = [ scmFoldNum "+" (+)
-             , scmFoldNum "-" (-)
-             , scmFoldNum "*" (*)
-             , scmFoldNum "/" div
+primitives = [ scmFoldNum  "+" (+)
+             , scmFoldNum  "-" (-)
+             , scmFoldNum  "*" (*)
+             , scmFoldFrac "/" (/)
              ]
 
-scmFoldNum :: String -> (Integer -> Integer -> Integer) -> (String, ScmValue)
-scmFoldNum s f = (s, ScmFunc s $ ScmInt . start)
+scmFoldNum :: String -> (forall a. Num a => a -> a -> a) -> (String, ScmValue)
+scmFoldNum s f = (s, ScmFunc s start)
  where
-   start []            = error $ "Wanted arguments for primitive " ++ s
-   start (ScmInt x:xs) = foldl' go x xs
-   start _             = badArg
+   start []     = error $ "Wanted arguments for primitive " ++ s
+   start (x:xs) =
+      if isNumeric x
+         then foldl' go x xs
+         else badArg
 
-   go n (ScmInt x) = f n x
-   go _ _          = badArg
+   go n x =
+      if isNumeric x
+         then liftScmNum2 f n x
+         else badArg
 
+   badArg = error $ "Nonnumeric argument to primitive " ++ s
+
+-- Like scmFoldNum but raises ScmInts to ScmRats
+scmFoldFrac :: String -> (forall a. Fractional a => a -> a -> a)
+            -> (String, ScmValue)
+scmFoldFrac s f = (s, ScmFunc s start)
+ where
+   start []               = error $ "Wanted arguments for primitive " ++ s
+   start (x:_) | notNum x = badArg
+   start (ScmInt x:xs)    = start (ScmRat (fromInteger x) : xs)
+   start (x:xs)           = foldl' go x xs
+
+   go _ x | notNum x = badArg
+   go n (ScmInt x)   = go n (ScmRat (fromInteger x))
+   go n x            =
+      if isNumeric x
+         then liftScmFrac2 f n x
+         else badArg
+
+   notNum = not . isNumeric
    badArg = error $ "Nonnumeric argument to primitive " ++ s
--- Haschoo/ScmValue.hs
+++ Haschoo/ScmValue.hs
@@ -1,5 +1,7 @@
 -- File created: 2009-07-11 21:47:19
 
+{-# LANGUAGE Rank2Types #-}
+
 module Haschoo.ScmValue where
 
 import Data.Complex        (Complex((:+)))
@@ -18,6 +20,13 @@ data ScmValue = ScmFunc    String ([ScmValue] -> ScmValue)
               | ScmComplex (Complex Double)
               | ScmList    [ScmValue]
  deriving Show
+
+isNumeric :: ScmValue -> Bool
+isNumeric (ScmInt     _) = True
+isNumeric (ScmRat     _) = True
+isNumeric (ScmReal    _) = True
+isNumeric (ScmComplex _) = True
+isNumeric _              = False
 
 scmShow :: ScmValue -> String
 scmShow (ScmBool b)   = '#' : if b then "t" else "f"
@@ -42,3 +51,51 @@ scmShow (ScmString s) = foldr ((.) . f) id s s
    f c | c == '\\' = showString "\\\\"
        | c == '\"' = showString "\\\""
        | otherwise = showChar c
+
+-- Return results with the more inexact constructor of the two given
+--
+-- Ugly and verbose... too lazy to metaize these
+liftScmNum2 :: (forall a. Num a => a -> a -> a)
+            -> (ScmValue -> ScmValue -> ScmValue)
+liftScmNum2 f (ScmInt     a) (ScmInt     b) = ScmInt    $ f a b
+liftScmNum2 f (ScmRat     a) (ScmRat     b) = ScmRat    $ f a b
+liftScmNum2 f (ScmReal    a) (ScmReal    b) = ScmReal   $ f a b
+liftScmNum2 f (ScmComplex a) (ScmComplex b) = ScmComplex$ f a b
+
+-- Int+{Rat,Real,Complex}
+liftScmNum2 f (ScmInt     a) (ScmRat     b) = ScmRat    $ f (fromInteger a) b
+liftScmNum2 f (ScmRat     a) (ScmInt     b) = ScmRat    $ f a (fromInteger b)
+liftScmNum2 f (ScmInt     a) (ScmReal    b) = ScmReal   $ f (fromInteger a) b
+liftScmNum2 f (ScmReal    a) (ScmInt     b) = ScmReal   $ f a (fromInteger b)
+liftScmNum2 f (ScmInt     a) (ScmComplex b) = ScmComplex$ f (fromInteger a) b
+liftScmNum2 f (ScmComplex a) (ScmInt     b) = ScmComplex$ f a (fromInteger b)
+
+-- Rat+{Real,Complex}
+liftScmNum2 f (ScmRat     a) (ScmReal    b) = ScmReal   $ f (fromRational a) b
+liftScmNum2 f (ScmReal    a) (ScmRat     b) = ScmReal   $ f a (fromRational b)
+liftScmNum2 f (ScmRat     a) (ScmComplex b) = ScmComplex$ f (fromRational a) b
+liftScmNum2 f (ScmComplex a) (ScmRat     b) = ScmComplex$ f a (fromRational b)
+
+-- Real+Complex
+liftScmNum2 f (ScmReal    a) (ScmComplex b) = ScmComplex$ f (a :+ 0) b
+liftScmNum2 f (ScmComplex a) (ScmReal    b) = ScmComplex$ f a (b :+ 0)
+
+liftScmNum2 _ _ _ = error "liftScmNum2 :: internal error"
+
+liftScmFrac2 :: (forall a. Fractional a => a -> a -> a)
+             -> (ScmValue -> ScmValue -> ScmValue)
+liftScmFrac2 f (ScmRat     a) (ScmRat     b) = ScmRat    $ f a b
+liftScmFrac2 f (ScmReal    a) (ScmReal    b) = ScmReal   $ f a b
+liftScmFrac2 f (ScmComplex a) (ScmComplex b) = ScmComplex$ f a b
+
+-- Rat+{Real,Complex}
+liftScmFrac2 f (ScmRat     a) (ScmReal    b) = ScmReal   $ f (fromRational a) b
+liftScmFrac2 f (ScmReal    a) (ScmRat     b) = ScmReal   $ f a (fromRational b)
+liftScmFrac2 f (ScmRat     a) (ScmComplex b) = ScmComplex$ f (fromRational a) b
+liftScmFrac2 f (ScmComplex a) (ScmRat     b) = ScmComplex$ f a (fromRational b)
+
+-- Real+Complex
+liftScmFrac2 f (ScmReal    a) (ScmComplex b) = ScmComplex$ f (a :+ 0) b
+liftScmFrac2 f (ScmComplex a) (ScmReal    b) = ScmComplex$ f a (b :+ 0)
+
+liftScmFrac2 _ _ _ = error "liftScmFrac2 :: internal error"
