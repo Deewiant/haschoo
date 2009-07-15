4aa49d423a814db95c8db3bef0195077227117ba
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2009-07-15 20:40:26 +0300

    (+) and (*) are defined as 0 and 1 respectively.

--- Haschoo/Evaluator/Primitives.hs
+++ Haschoo/Evaluator/Primitives.hs
@@ -4,25 +4,27 @@
 
 module Haschoo.Evaluator.Primitives (primitives) where
 
-import Data.List (foldl')
+import Data.List (foldl1')
 
 import Haschoo.ScmValue (ScmValue(..), isNumeric, liftScmFrac2, liftScmNum2)
 
 primitives :: [(String, ScmValue)]
-primitives = [ scmFoldNum  "+" (+)
-             , scmFoldNum  "-" (-)
-             , scmFoldNum  "*" (*)
-             , scmFoldFrac "/" (/)
+primitives = [ scmFoldNum  "+" (+) (const 0)
+             , scmFoldNum  "-" (-) tooFewArgs
+             , scmFoldNum  "*" (*) (const 1)
+             , scmFoldFrac "/" (/) tooFewArgs
              ]
-
-scmFoldNum :: String -> (forall a. Num a => a -> a -> a) -> (String, ScmValue)
-scmFoldNum s f = (s, ScmFunc s start)
  where
-   start []     = error $ "Wanted arguments for primitive " ++ s
-   start (x:xs) =
-      if isNumeric x
-         then foldl' go x xs
-         else badArg
+   tooFewArgs = error . ("Wanted arguments for primitive " ++)
+
+scmFoldNum :: String
+           -> (forall a. Num a => a -> a -> a)
+           -> (String -> Integer)
+           -> (String, ScmValue)
+scmFoldNum s f empty = (s, ScmFunc s start)
+ where
+   start [] = ScmInt $ empty s
+   start xs = foldl1' go xs
 
    go n x =
       if isNumeric x
@@ -32,14 +34,16 @@ scmFoldNum s f = (s, ScmFunc s start)
    badArg = error $ "Nonnumeric argument to primitive " ++ s
 
 -- Like scmFoldNum but raises ScmInts to ScmRats
-scmFoldFrac :: String -> (forall a. Fractional a => a -> a -> a)
+scmFoldFrac :: String
+            -> (forall a. Fractional a => a -> a -> a)
+            -> (String -> Integer)
             -> (String, ScmValue)
-scmFoldFrac s f = (s, ScmFunc s start)
+scmFoldFrac s f empty = (s, ScmFunc s start)
  where
-   start []               = error $ "Wanted arguments for primitive " ++ s
+   start []               = ScmInt $ empty s
    start (x:_) | notNum x = badArg
    start (ScmInt x:xs)    = start (ScmRat (fromInteger x) : xs)
-   start (x:xs)           = foldl' go x xs
+   start (x:xs)           = foldl1' go xs
 
    go _ x | notNum x = badArg
    go n (ScmInt x)   = go n (ScmRat (fromInteger x))
