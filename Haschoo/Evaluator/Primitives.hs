b7fb4ac611c072829c7a6c7833eeb3012b0f1427
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2009-07-15 20:51:55 +0300

    Lose scmFold* from Primitives: simplifies.

--- Haschoo/Evaluator/Primitives.hs
+++ Haschoo/Evaluator/Primitives.hs
@@ -1,56 +1,43 @@
 -- File created: 2009-07-15 11:00:10
 
-{-# LANGUAGE Rank2Types #-}
-
 module Haschoo.Evaluator.Primitives (primitives) where
 
-import Data.List (foldl1')
+import Data.List (foldl', foldl1')
 
 import Haschoo.ScmValue (ScmValue(..), isNumeric, liftScmFrac2, liftScmNum2)
 
 primitives :: [(String, ScmValue)]
-primitives = [ scmFoldNum  "+" (+) (const 0)
-             , scmFoldNum  "-" (-) tooFewArgs
-             , scmFoldNum  "*" (*) (const 1)
-             , scmFoldFrac "/" (/) tooFewArgs
-             ]
+primitives = map (\(a,b) -> (a, ScmFunc a b)) $
+   [ ("+", scmPlus)
+   , ("-", scmMinus)
+   , ("*", scmMul)
+   , ("/", scmDiv)
+   ]
+
+scmPlus, scmMinus, scmMul, scmDiv :: [ScmValue] -> ScmValue
+scmPlus [] = ScmInt 0
+scmPlus xs = foldl1' go xs
  where
-   tooFewArgs = error . ("Wanted arguments for primitive " ++)
+   go n x = if isNumeric x then liftScmNum2 (+) n x else notNum "+"
 
-scmFoldNum :: String
-           -> (forall a. Num a => a -> a -> a)
-           -> (String -> Integer)
-           -> (String, ScmValue)
-scmFoldNum s f empty = (s, ScmFunc s start)
+scmMinus [] = tooFewArgs "-"
+scmMinus xs = foldl1' go xs
  where
-   start [] = ScmInt $ empty s
-   start xs = foldl1' go xs
+   go n x = if isNumeric x then liftScmNum2 (-) n x else notNum "-"
 
-   go n x =
-      if isNumeric x
-         then liftScmNum2 f n x
-         else badArg
-
-   badArg = error $ "Nonnumeric argument to primitive " ++ s
-
--- Like scmFoldNum but raises ScmInts to ScmRats
-scmFoldFrac :: String
-            -> (forall a. Fractional a => a -> a -> a)
-            -> (String -> Integer)
-            -> (String, ScmValue)
-scmFoldFrac s f empty = (s, ScmFunc s start)
+scmMul [] = ScmInt 1
+scmMul xs = foldl1' go xs
  where
-   start []               = ScmInt $ empty s
-   start (x:_) | notNum x = badArg
-   start (ScmInt x:xs)    = start (ScmRat (fromInteger x) : xs)
-   start (x:xs)           = foldl1' go xs
+   go n x = if isNumeric x then liftScmNum2 (*) n x else notNum "*"
 
-   go _ x | notNum x = badArg
-   go n (ScmInt x)   = go n (ScmRat (fromInteger x))
-   go n x            =
-      if isNumeric x
-         then liftScmFrac2 f n x
-         else badArg
+scmDiv []     = tooFewArgs "/"
+scmDiv (x:xs) = foldl' go (unint x) xs
+ where
+   go n x = if isNumeric x then liftScmFrac2 (/) n (unint x) else notNum "/"
 
-   notNum = not . isNumeric
-   badArg = error $ "Nonnumeric argument to primitive " ++ s
+   unint (ScmInt x) = ScmRat (fromInteger x)
+   unint x          = x
+
+notNum, tooFewArgs :: String -> a
+notNum     = error . ("Nonnumeric argument to primitive procedure " ++)
+tooFewArgs = error . ("No arguments to primitive procedure " ++)
