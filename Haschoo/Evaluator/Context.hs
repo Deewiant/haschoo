f41468e004037299fd5f461a51ce71ec0ffbcd0f
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2009-07-19 19:09:39 +0300

    Separate the Context type and topContext.

--- Haschoo/Evaluator/Context.hs
+++ Haschoo/Evaluator/Context.hs
@@ -1,23 +1,22 @@
 -- File created: 2009-07-15 10:46:58
 
-module Haschoo.Evaluator.Context where
+module Haschoo.Evaluator.Context (Context(..), mkContext) where
 
 import           Data.IntMap                     (IntMap)
 import qualified Data.IntMap as IM
 import           Data.ListTrie.Patricia.Map.Enum (TrieMap)
 import qualified Data.ListTrie.Patricia.Map.Enum as TM
 
-import Haschoo.ScmValue           (ScmValue)
-import Haschoo.Evaluator.Standard (procedures)
-import Haschoo.Utils              (swap)
+import Haschoo.ScmValue (ScmValue)
+import Haschoo.Utils    (swap)
 
 data Context = Context { idMap  :: TrieMap Char Int
                        , valMap :: IntMap ScmValue }
  deriving Show
 
-topContext :: Context
-topContext = Context ids vals
+mkContext :: [(String, ScmValue)] -> Context
+mkContext namedVals = Context ids vals
  where
    key   = zip [0..]
-   vals  = IM.fromList .            key . map snd $ procedures
-   ids   = TM.fromList . map swap . key . map fst $ procedures
+   vals  = IM.fromList .            key . map snd $ namedVals
+   ids   = TM.fromList . map swap . key . map fst $ namedVals
--- Haschoo/Evaluator/Standard.hs
+++ Haschoo/Evaluator/Standard.hs
@@ -1,9 +1,13 @@
 -- File created: 2009-07-15 11:00:10
 
-module Haschoo.Evaluator.Standard (procedures) where
+module Haschoo.Evaluator.Standard (context) where
 
-import Haschoo.ScmValue (ScmValue)
+import           Haschoo.ScmValue          (ScmValue)
+import           Haschoo.Evaluator.Context (Context, mkContext)
 import qualified Haschoo.Evaluator.Standard.Numeric as Numeric
 
 procedures :: [(String, ScmValue)]
 procedures = Numeric.procedures
+
+context :: Context
+context = mkContext procedures
