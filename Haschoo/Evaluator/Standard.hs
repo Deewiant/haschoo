d451b79c3326a0b413019a4dfa134c5b524f1b3d
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2009-07-21 22:09:30 +0300

    Add: not boolean?

--- Haschoo/Evaluator/Standard.hs
+++ Haschoo/Evaluator/Standard.hs
@@ -3,12 +3,16 @@
 module Haschoo.Evaluator.Standard (context) where
 
 import           Haschoo.Types (ScmValue, Context, mkContext)
+import qualified Haschoo.Evaluator.Standard.Boolean     as Boolean
 import qualified Haschoo.Evaluator.Standard.Equivalence as Equivalence
 import qualified Haschoo.Evaluator.Standard.IO          as IO
 import qualified Haschoo.Evaluator.Standard.Numeric     as Numeric
 
 procedures :: [(String, ScmValue)]
-procedures = concat [Equivalence.procedures, IO.procedures, Numeric.procedures]
+procedures = concat [ Boolean.procedures
+                    , Equivalence.procedures
+                    , IO.procedures
+                    , Numeric.procedures ]
 
 context :: Context
 context = mkContext procedures
---
+++ Haschoo/Evaluator/Standard/Boolean.hs
@@ -0,0 +1,23 @@
+-- File created: 2009-07-21 22:04:14
+
+module Haschoo.Evaluator.Standard.Boolean (procedures) where
+
+import Haschoo.Types           (ScmValue(..))
+import Haschoo.Utils           (ErrOr, (.:))
+import Haschoo.Evaluator.Utils (tooFewArgs, tooManyArgs)
+
+procedures :: [(String, ScmValue)]
+procedures = map (\(a,b) -> (a, ScmFunc a (return. b))) $
+   [ ("not",      fmap ScmBool . scmNot)
+   , ("boolean?", fmap ScmBool . scmIsBoolean) ]
+
+scmNot, scmIsBoolean :: [ScmValue] -> ErrOr Bool
+scmNot [ScmBool False] = Right True
+scmNot [_]             = Right False
+scmNot []              = tooFewArgs  "not"
+scmNot _               = tooManyArgs "not"
+
+scmIsBoolean [ScmBool _] = Right True
+scmIsBoolean [_]         = Right False
+scmIsBoolean []          = tooFewArgs  "boolean?"
+scmIsBoolean _           = tooManyArgs "boolean?"
