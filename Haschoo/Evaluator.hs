6157fadd4e1a5cf187e314571e2ecbe56d13e092
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2009-07-26 16:53:04 +0300

    Add define-syntax.

--- Haschoo/Evaluator.hs
+++ Haschoo/Evaluator.hs
@@ -2,9 +2,14 @@
 
 module Haschoo.Evaluator (evalToplevel) where
 
+import Control.Monad.Error (throwError)
+import Control.Monad.State (get)
 
-import Haschoo.Types            (Haschoo, ScmValue)
-import Haschoo.Evaluator.Eval   (evalBody)
+import Haschoo.Types            ( Haschoo
+                                , ScmValue(ScmList, ScmIdentifier, ScmSyntax))
+import Haschoo.Evaluator.Eval   (eval, evalBody, define)
+import Haschoo.Evaluator.Macros (mkMacro)
+import Haschoo.Evaluator.Utils  (tooFewArgs, tooManyArgs)
 
 -- Programs consist of three things:
 --    expressions        - valid anywhere
@@ -19,6 +24,22 @@ import Haschoo.Evaluator.Eval   (evalBody)
 -- hence definitions are handled separately in the former two instead of being
 -- ordinary primitives.
 
--- TODO: syntax definitions
 evalToplevel :: [ScmValue] -> Haschoo ScmValue
-evalToplevel = fmap last . mapM (evalBody . return)
+evalToplevel (ScmList (ScmIdentifier "define-syntax":xs) : ds) =
+   scmDefineSyntax xs >> evalToplevel ds
+
+evalToplevel ds = fmap last . mapM (evalBody . return) $ ds
+
+scmDefineSyntax :: [ScmValue] -> Haschoo ()
+scmDefineSyntax [ScmIdentifier var, x] = do
+   x' <- eval x
+   case x' of
+        ScmSyntax pats lits -> do
+           ctx <- get
+           define var (mkMacro ctx var pats lits)
+
+        _ -> throwError "define-syntax :: expected instance of syntax-rules"
+
+scmDefineSyntax [_,_]   = throwError  "define-syntax :: expected identifier"
+scmDefineSyntax (_:_:_) = tooManyArgs "define-syntax"
+scmDefineSyntax _       = tooFewArgs  "define-syntax"
