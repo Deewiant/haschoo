37202a1939f3aff11cec64416e91be63f37cb387
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2009-08-09 19:21:29 +0300

    Add two currently failing macro-related tests.

---
+++ tests/macros/dottedlist-ellipsis.scm
@@ -0,0 +1,6 @@
+(define-syntax m (syntax-rules ()
+  ((m (x . y) ...) (append (list x ...) (list y ...)))))
+
+(letrec ((x (m (1 . 2) (3 . 4))))
+  (if (equal? x '(1 3 2 4))
+      (begin (write 'ok)(newline))))
