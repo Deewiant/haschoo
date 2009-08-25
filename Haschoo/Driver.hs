2b5adbb78b28c7f4b0c92002b1bb228ba4164b62
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2009-08-26 01:13:47 +0300

    Add a simple REPL.

--- Haschoo/Driver.hs
+++ Haschoo/Driver.hs
@@ -7,9 +7,8 @@ import Prelude hiding (catch)
 import Control.Exception  (catch)
 import Data.IORef         (newIORef, readIORef)
 import System.Environment (getArgs)
-import System.IO          (stdin)
 
-import Haschoo.Running (runHandle, runFile, run, RunError)
+import Haschoo.Running (runRepl, runFile, run, RunError)
 import Haschoo.Stdlib  (toplevelContext)
 import Haschoo.Utils   (void, errPrint)
 
@@ -18,7 +17,7 @@ main = do
    ctx  <- toplevelContext
    args <- getArgs
    if null args
-      then runHandle ctx "<stdin>" stdin
+      then runRepl ctx
       else do
          initCtx <- mapM readIORef ctx
          mapM_ (\f -> do
--- Haschoo/Running.hs
+++ Haschoo/Running.hs
@@ -1,18 +1,20 @@
 -- File created: 2009-07-26 17:17:23
 
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveDataTypeable, ScopedTypeVariables #-}
+module Haschoo.Running (RunError(..), runFile, runHandle, run, runRepl) where
 
-module Haschoo.Running (RunError(..), runFile, runHandle, run) where
-
-import Control.Exception (Exception, throw)
-import Control.Monad     ((>=>))
+import Control.Exception (Exception, throw, catch)
+import Control.Monad     ((>=>), when)
 import Data.IORef        (IORef)
 import Data.Typeable     (Typeable)
-import System.IO         (Handle, openFile, IOMode(ReadMode), hGetContents)
+import System.IO         ( Handle, openFile, IOMode(ReadMode), hGetContents
+                         , hPutStrLn, hFlush, stderr, stdout )
+
+import Prelude hiding (catch)
 
 import Haschoo.Evaluator (evalToplevel)
 import Haschoo.Parser    (runParser, programValue)
-import Haschoo.Types     (Context)
+import Haschoo.Types     (Context, scmShow)
 
 data RunError = ParseError String | RuntimeError String
  deriving Typeable
@@ -32,7 +34,7 @@ runHandle ctx name = hGetContents >=> run ctx name
 run :: [IORef Context] -> FilePath -> String -> IO ()
 run ctx name = go Nothing
  where
-   go pos str = do
+   go pos str =
       case runParser (programValue pos) name str of
            Left e               -> throw (ParseError e)
            Right (Just (v,s,p)) -> do
@@ -40,5 +42,27 @@ run ctx name = go Nothing
               case result of
                    Left  e -> throw (RuntimeError e)
                    Right _ -> go (Just p) s
+
+           Right Nothing -> return ()
+
+runRepl :: [IORef Context] -> IO ()
+runRepl ctx = do
+   putStr "Haschoo} "
+   hFlush stdout
+   line <- fmap Just getLine `catch` (\(_ :: IOError) -> return Nothing)
+   maybe (return ()) (\l -> handle (go l) >> runRepl ctx) line
+ where
+   handle = flip catch $ \e -> hPutStrLn stderr $ show (e :: RunError)
+   go str =
+      case runParser (programValue Nothing) "<repl>" str of
+           Left e               -> throw (ParseError e)
+           Right (Just (v,s,_)) -> do
+              result <- evalToplevel ctx v
+              case result of
+                   Left  e -> throw (RuntimeError e)
+                   Right r -> do
+                      rs <- scmShow r
+                      when (not $ null rs) (putStrLn rs)
+                      go s
 
            Right Nothing -> return ()
